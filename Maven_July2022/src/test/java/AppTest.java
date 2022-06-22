c26420747cc56245d86f758f9deeb6328f667c80
ShashiChauhan1 <107924881+ShashiChauhan1@users.noreply.github.com>
2022-06-22 12:00:31 +0100

    NEw test

---
+++ Maven_July2022/src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void test2()
+    {
+        System.out.println("Test2");
+    }
+}
