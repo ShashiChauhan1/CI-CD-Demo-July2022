d16012414aefb0cb8bc0ca2475f8d2697f885330
ShashiChauhan1 <107924881+ShashiChauhan1@users.noreply.github.com>
2022-06-22 12:00:31 +0100

    tkt no 14.44

--- Maven_July2022/src/main/java/Application.java
+++ Maven_July2022/src/main/java/Application.java
@@ -1,7 +1,6 @@
 public class Application {
 
     public static void main(String[] args) {
-
-        System.out.println("Application is running fine today on");
+        System.out.println("Application is running fine now");
     }
 }
