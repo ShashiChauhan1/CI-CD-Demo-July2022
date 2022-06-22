3701d2e2f670ff2a2ca4804e2ecb972ee094439d
ShashiChauhan1 <107924881+ShashiChauhan1@users.noreply.github.com>
2022-06-22 15:39:22 +0100

    tkt1540

--- Maven_June/src/test/java/AppTest.java
+++ Maven_June/src/test/java/AppTest.java
@@ -13,4 +13,9 @@ public class AppTest {
     {
         System.out.println("test2 added at 15.20");
     }
+    @Test
+    public void test2()
+    {
+        System.out.println("test2 added at 15.40");
+    }
 }
